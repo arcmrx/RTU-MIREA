198c87314ecf0129d4941f7a971f50fcb70dbff0
arcmrx <arturvolkov44@gmail.com>
2024-03-19 00:43:47 +0300

    Siaod 2 - Done

--- 2 semestr/siaod/siaod_2.cpp
+++ 2 semestr/siaod/siaod_2.cpp
@@ -1,159 +1,128 @@
 #include <iostream>
-#include <cmath>
 
-struct TypeSomething
+typedef unsigned int typeitem;
+const unsigned int N = 100;
+
+struct typeX
 {
-    int size;
-    int *arr;
-    bool inserted_is_7_root = false;
-    int inserted_index;
+    unsigned int size = 0;
+    typeitem array[N] = {};
+};
 
-    void input_array()
+void input_array(typeX *arr)
+{
+    std::cout << "Введите размер массива: \n";
+    std::cin >> arr->size;
+
+    std::cout << "Введите элементы массива: \n";
+    for (int i = 0; i < arr->size; i++)
     {
-        std::cout << "Введите размер массива: \n";
-        std::cin >> size;
-        arr = new int[size];
-        std::cout << "Введите элементы массива: \n";
-        for (int i = 0; i < size; i++)
+        std::cin >> arr->array[i];
+    }
+}
+
+void output_array(typeX *arr)
+{
+    std::cout << "Массив: \n";
+    for (int i = 0; i < arr->size; i++)
+    {
+        std::cout << arr->array[i] << " ";
+    }
+    std::cout << "\n";
+}
+
+int digitalRoot(int num)
+{
+    while (num > 9)
+    {
+        int sum = 0;
+        while (num > 0)
         {
-            std::cin >> arr[i];
+            sum += num % 10;
+            num /= 10;
+        }
+        num = sum;
+    }
+    return num;
+}
+
+int findPosition(typeX *arr)
+{
+    for (int i = 0; i < arr->size; ++i)
+    {
+        if (digitalRoot(arr->array[i]) == 7)
+        {
+            return i;
         }
     }
+    return -1;
+}
 
-    void output_array()
+void insertBefore7(typeX *arr)
+{
+    int new_elem;
+    std::cout << "Введите вставляемый элемент: \n";
+    std::cin >> new_elem;
+    if (digitalRoot(new_elem) == 7)
     {
-        std::cout << "Массив: \n";
-        for (int i = 0; i < size; i++)
-        {
-            std::cout << arr[i] << " ";
-        }
-        std::cout << "\n";
+        std::cout << "Новый элемент имеет цифровой корень 7." << std::endl;
+        return;
     }
-
-    int digitalRoot(int num)
+    int position = findPosition(arr);
+    if (position == -1)
     {
-        while (num > 9)
-        {
-            int sum = 0;
-            while (num > 0)
-            {
-                sum += num % 10;
-                num /= 10;
-            }
-            num = sum;
-        }
-        return num;
+        std::cout << "Элемент с цифровым корнем 7 не найден." << std::endl;
+        return;
     }
-
-    int findPosition()
+    typeX newArr;
+    newArr.size = arr->size + 1;
+    for (int i = 0, j = 0; i < newArr.size; ++i)
     {
-        for (int i = 0; i < size; ++i)
+        if (i == position)
         {
-            if (digitalRoot(arr[i]) == 7)
-            {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    void insertBefore7()
-    {
-        int new_elem;
-        std::cout << "Введите вставляемый элемент: \n";
-        std::cin >> new_elem;
-        if (digitalRoot(new_elem) == 7)
-        {
-            inserted_is_7_root = true;
-        }
-        int position = findPosition();
-        if (position == -1)
-        {
-            std::cout << "Элемент с цифровым корнем 7 не найден." << std::endl;
-            return;
-        }
-        inserted_index = position;
-        size++;
-        int *new_arr = new int[size];
-        for (int i = 0; i < size; i++)
-        {
-            if (i < position)
-            {
-                new_arr[i] = arr[i];
-            }
-            else if (i == position)
-            {
-                new_arr[i] = new_elem;
-            }
-            else
-            {
-                new_arr[i] = arr[i - 1];
-            }
-        }
-        delete[] arr;
-        arr = new_arr;
-    }
-
-    void remove7RootElem()
-    {
-        int position = findPosition();
-        if (position == -1)
-        {
-            std::cout << "Элемент с цифровым корнем 7 не найден." << std::endl;
-            return;
-        }
-        if (!inserted_is_7_root)
-        {
-            size--;
-            int *new_arr = new int[size];
-            for (int i = 0; i < size; i++)
-            {
-                if (i < position)
-                {
-                    new_arr[i] = arr[i];
-                }
-                else
-                {
-                    new_arr[i] = arr[i + 1];
-                }
-            }
-            delete[] arr;
-            arr = new_arr;
+            newArr.array[i] = new_elem;
         }
         else
         {
-            size -= 2;
-            int *new_arr = new int[size];
-            for (int i = 0; i < size; i++)
-            {
-                if (i < position && i < inserted_index)
-                {
-                    new_arr[i] = arr[i];
-                }
-                else if (i < position || i < inserted_index)
-                {
-                    new_arr[i] = arr[i + 1];
-                }
-                else
-                {
-                    new_arr[i] = arr[i + 2];
-                }
-            }
-            delete[] arr;
-            arr = new_arr;
+            newArr.array[i] = arr->array[j++];
         }
-        std::cout << "Массив после удаления элемент:\n";
     }
-};
+    *arr = newArr;
+    std::cout << "Элемент успешно вставлен перед 7.\n";
+}
+
+void remove7RootElem(typeX *arr)
+{
+    int position = findPosition(arr);
+    if (position == -1)
+    {
+        std::cout << "Элемент с цифровым корнем 7 не найден." << std::endl;
+        return;
+    }
+    typeX newArr;
+    newArr.size = arr->size - 1;
+    for (int i = 0, j = 0; i < newArr.size; ++i)
+    {
+        if (j == position)
+        {
+            ++j;
+        }
+        newArr.array[i] = arr->array[j++];
+    }
+    *arr = newArr;
+    std::cout << "Элемент успешно удален.\n";
+}
 
 int main()
 {
-    TypeSomething array;
-    array.input_array();
-    array.output_array();
-    array.insertBefore7();
-    array.output_array();
-    array.remove7RootElem();
-    array.output_array();
+    setlocale(LC_ALL, "");
+    typeX arr;
+
+    input_array(&arr);
+    output_array(&arr);
+    insertBefore7(&arr);
+    output_array(&arr);
+    remove7RootElem(&arr);
+    output_array(&arr);
     return 0;
 }
