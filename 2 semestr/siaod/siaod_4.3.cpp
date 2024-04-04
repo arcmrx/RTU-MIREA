7bc0850b5197a306e6536976ac6b38543aa46f9c
arcmrx <arturvolkov44@gmail.com>
2024-04-04 17:08:06 +0300

    4

--- 2 semestr/siaod/siaod_4.3.cpp
+++ 2 semestr/siaod/siaod_4.3.cpp
@@ -1,73 +1,87 @@
 #include <iostream>
+#include <vector>
+#include <cmath>
 
-struct Matrix
+struct Point
 {
-    int size;
-    int data[100][100];
+    double x;
+    double y;
 };
 
-void printClockwise(const Matrix &matrix)
+void fillPointsVector(std::vector<Point> &points, int size)
 {
-    int top = 0, bottom = matrix.size - 1;
-    int left = 0, right = matrix.size - 1;
-
-    while (top <= bottom && left <= right)
+    points.resize(size);
+    std::cout << "Enter points:\n";
+    for (int i = 0; i < size; ++i)
     {
-        for (int i = left; i <= right; ++i)
-        {
-            std::cout << matrix.data[top][i] << " ";
-        }
-        ++top;
-
-        for (int i = top; i <= bottom; ++i)
-        {
-            std::cout << matrix.data[i][right] << " ";
-        }
-        --right;
-
-        if (top <= bottom)
-        {
-            for (int i = right; i >= left; --i)
-            {
-                std::cout << matrix.data[bottom][i] << " ";
-            }
-            --bottom;
-        }
-
-        if (left <= right)
-        {
-            for (int i = bottom; i >= top; --i)
-            {
-                std::cout << matrix.data[i][left] << " ";
-            }
-            ++left;
-        }
+        std::cin >> points[i].x >> points[i].y;
     }
 }
 
-Matrix inputMatrix()
+bool isBalancedTriangle(const Point &p1, const Point &p2, const Point &p3, const std::vector<Point> &set1, const std::vector<Point> &set2)
 {
-    Matrix matrix;
-    std::cout << "Введите размер квадратной матрицы: ";
-    std::cin >> matrix.size;
-
-    std::cout << "Введите элементы матрицы:\n";
-    for (int i = 0; i < matrix.size; ++i)
+    int set1Count = 0, set2Count = 0;
+    for (const Point &point : set1)
     {
-        for (int j = 0; j < matrix.size; ++j)
+        double area = 0.5 * ((p2.x - p1.x) * (p3.y - p1.y) - (p3.x - p1.x) * (p2.y - p1.y));
+        double area1 = 0.5 * ((point.x - p1.x) * (p2.y - p1.y) - (p2.x - p1.x) * (point.y - p1.y));
+        double area2 = 0.5 * ((p2.x - p1.x) * (point.y - p1.y) - (point.x - p1.x) * (p2.y - p1.y));
+        double area3 = 0.5 * ((point.x - p1.x) * (p3.y - p1.y) - (p3.x - p1.x) * (point.y - p1.y));
+        if ((area1 >= 0 && area2 >= 0 && area3 >= 0) || (area1 <= 0 && area2 <= 0 && area3 <= 0))
         {
-            std::cin >> matrix.data[i][j];
+            ++set1Count;
         }
     }
-    return matrix;
+    for (const Point &point : set2)
+    {
+        double area = 0.5 * ((p2.x - p1.x) * (p3.y - p1.y) - (p3.x - p1.x) * (p2.y - p1.y));
+        double area1 = 0.5 * ((point.x - p1.x) * (p2.y - p1.y) - (p2.x - p1.x) * (point.y - p1.y));
+        double area2 = 0.5 * ((p2.x - p1.x) * (point.y - p1.y) - (point.x - p1.x) * (p2.y - p1.y));
+        double area3 = 0.5 * ((point.x - p1.x) * (p3.y - p1.y) - (p3.x - p1.x) * (point.y - p1.y));
+        if ((area1 >= 0 && area2 >= 0 && area3 >= 0) || (area1 <= 0 && area2 <= 0 && area3 <= 0))
+        {
+            ++set2Count;
+        }
+    }
+    return set1Count == set2Count;
+}
+
+void findBalancedTriangle(const std::vector<Point> &points1, const std::vector<Point> &points2)
+{
+    int n = points1.size();
+    for (int i = 0; i < n - 2; ++i)
+    {
+        for (int j = i + 1; j < n - 1; ++j)
+        {
+            for (int k = j + 1; k < n; ++k)
+            {
+                if (isBalancedTriangle(points1[i], points1[j], points1[k], points1, points2))
+                {
+                    std::cout << "Found balanced triangle:\n";
+                    std::cout << "(" << points1[i].x << ", " << points1[i].y << "), ";
+                    std::cout << "(" << points1[j].x << ", " << points1[j].y << "), ";
+                    std::cout << "(" << points1[k].x << ", " << points1[k].y << ")\n";
+                    return;
+                }
+            }
+        }
+    }
+    std::cout << "No balanced triangle found.\n";
 }
 
 int main()
 {
-    Matrix matrix = inputMatrix();
+    int size1, size2;
+    std::cout << "Enter the number of points in the first set: ";
+    std::cin >> size1;
+    std::cout << "Enter the number of points in the second set: ";
+    std::cin >> size2;
 
-    std::cout << "Элементы матрицы по часовой стрелке начиная с первой строки:\n";
-    printClockwise(matrix);
+    std::vector<Point> points1, points2;
+    fillPointsVector(points1, size1);
+    fillPointsVector(points2, size2);
+
+    findBalancedTriangle(points1, points2);
 
     return 0;
 }
